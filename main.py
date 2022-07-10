fed3921eadb6b08bd7ab4fce5781312a2945720d
Dodgo1 <91613930+Dodgo1@users.noreply.github.com>
2022-07-10 14:45:35 +0200

    hash check + env fix

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from pprint import pprint
 import dotenv
 from slugify import slugify
 import os
+import hashlib
 
 dotenv.load_dotenv()
 USERNAME = os.getenv("UNAME")
@@ -32,18 +33,34 @@ for post in posts:
 
     if post['data']['subreddit_name_prefixed'] == "r/wallpaper":
         title = str(post['data']['title']).replace(" ", "-")
+
         if 'is_gallery' in post['data']:
+
             for img_meta in post['data']['media_metadata']:
                 url = f"https://i.redd.it/{img_meta}.jpg"
                 file_name = slugify(f"{title}-{img_meta}")
                 path = f"Wallpapers/{file_name}.png"
-                img_data = requests.get(url).content
-                with open(path, 'wb') as handler:
-                    handler.write(img_data)
+                img_new_data = requests.get(url).content
+                try:
+                    with open(path, 'xb') as handler:
+                        handler.write(img_new_data)
+                        print("🤯 New file added")
+                except FileExistsError:
+                    print("🥵File already exists")
+                    with open(path, 'rb') as img_exist_bytes:
+                        exist_hash = hashlib.sha1(img_exist_bytes.read()).hexdigest()
+                        new_hash = hashlib.sha1(img_new_data).hexdigest()
+                        if not exist_hash == new_hash:
+                            with open(path, 'wb') as img_write:
+                                print("🥶Different hashes - updating img")
+                                img_write.write(img_new_data)
+                        elif exist_hash == new_hash:
+                            print("🧠Same hashes")
+
             continue
 
         path = f"Wallpapers/{slugify(title)}.png"
         url = post['data']['url_overridden_by_dest']
-        img_data = requests.get(url).content
+        img_new_data = requests.get(url).content
         with open(path, 'wb') as handler:
-            handler.write(img_data)
+            handler.write(img_new_data)
